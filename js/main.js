10c1b422983fb6cae71b8749217354169e0f0ec9
CynthiaA <cynthiaesperanzaavilaperez@gmail.com>
2023-12-05 18:23:20 -0600

    Validations in the JS were updated and changes were made in the CSS

--- js/main.js
+++ js/main.js
@@ -19,26 +19,76 @@ function linkAction() {
 }
 navLink.forEach((item) => item.addEventListener("click", linkAction));
 
+/*<---Formulario----->*/
 
 const clearButton = document.getElementById("ClearButton");
 if (clearButton) {
   clearButton.addEventListener("click", () => {
-   
     const fullNameInput = document.getElementById("FullName");
     const emailInput = document.getElementById("Email");
     const messageInput = document.getElementById("Message");
 
- 
     fullNameInput.value = "";
     emailInput.value = "";
     messageInput.value = "";
   });
 }
 
+/*<---Expresiones----->*/
+
+const regexName = /^[a-zA-Z ]{3,30}$/;
+const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+const regexMessage = /.+/; // Simple check for non-empty
+
 const submitButton = document.getElementById("SubmitButton");
 if (submitButton) {
-  submitButton.addEventListener("click", () => {
- 
-    alert("Hello, your message is sent! I will respond as soon as posible <3");
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault(); // Previene la acción por defecto del formulario
+
+    const fullNameInput = document.getElementById("FullName");
+    const emailInput = document.getElementById("Email");
+    const messageInput = document.getElementById("Message");
+
+    // Validación de campos
+
+    if(fullNameInput.value === "") {
+      alert("Please enter your full name.");
+      fullNameInput.value = "";
+      return;
+  } else if (!regexName.test(fullNameInput.value)) {
+      alert("The entered name is not valid.");
+      fullNameInput.value = ""; 
+      return;
+  }
+
+  if(emailInput.value === "") {
+      alert("Please enter your email address.");
+      emailInput.value = "";
+      return;
+  } else if (!regexEmail.test(emailInput.value)) {
+      alert("The entered email address is not valid.");
+      emailInput.value = "";
+
+      return;
+  }
+
+  if(messageInput.value === "") {
+      alert("Please enter your message.");
+      messageInput.value = "";
+      return;
+  } else if (!regexMessage.test(messageInput.value)) {
+      alert("The entered message is not valid.");
+      messageInput.value = "";
+      return;
+  }
+
+  fullNameInput.value = "";
+  emailInput.value = "";
+  messageInput.value = "";
+
+    // Si todo está bien.. 
+    alert("Hello, your message is sent! I will respond as soon as possible <3");
+
+
   });
 }
